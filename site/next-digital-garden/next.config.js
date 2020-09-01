8da903279e8f802f1bc9f8f8298993633519b397
talves <tony@3alves.com>
2020-09-01 14:42:53 -0700

    config watch and test

--- site/next-digital-garden/next.config.js
+++ site/next-digital-garden/next.config.js
@@ -1,7 +1,27 @@
-const withMDX = require("@next/mdx")({
+const {
+  default: WatchExternalFilesPlugin,
+} = require('webpack-watch-files-plugin')
+
+const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
-});
+})
 
 module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "mdx"],
-});
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // config.plugins.push(new webpack.IgnorePlugin(/\/public\//));
+    // config.plugins.push(new webpack.IgnorePlugin(/jsconfig.*/));
+    // config.plugins.push(new webpack.IgnorePlugin(/.\/*.lock/));
+    config.plugins.push(
+      new WatchExternalFilesPlugin({
+        files: ['./garden/**/*.mdx'],
+      }),
+    )
+
+    // console.log(JSON.stringify(config, null, 2));
+    // Important: return the modified config
+    return config
+  },
+})
--- site/next-digital-garden/pages/_document.js
+++ site/next-digital-garden/pages/_document.js
@@ -1,10 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+  /* Uncomment below if you need to change the initial props */
+  // static async getInitialProps(ctx) {
+  //   const initialProps = await Document.getInitialProps(ctx)
+  //   return { ...initialProps }
+  // }
 
   render() {
     return (
