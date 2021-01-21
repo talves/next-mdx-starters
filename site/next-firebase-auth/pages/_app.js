7145b2130e2d13b7fcbb840a4d250baae2c85246
talves <tony@3alves.com>
2021-01-21 15:58:46 -0800

    wrapper

--- site/next-firebase-auth/pages/_app.js
+++ site/next-firebase-auth/pages/_app.js
@@ -1,7 +1,21 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import { FirebaseProvider } from "firebase-react-provider";
+
+/* Replace the config values with your firebase config */
+const config = {
+  apiKey: "AIza...................................",
+  authDomain: "your-app-name.firebaseapp.com",
+  databaseURL: "https://your-app-name.firebaseio.com",
+  projectId: "your-app-name",
+  // storageBucket: "your-app-name.appspot.com",
+};
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FirebaseProvider config={config}>
+      <Component {...pageProps} />
+    </FirebaseProvider>
+  );
 }
 
-export default Application
+export default Application;
