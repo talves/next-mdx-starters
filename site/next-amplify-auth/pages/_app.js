5f4cdd1df560ea1d7db6b8b78add7b54a8c40851
talves <tony@3alves.com>
2021-01-22 15:52:45 -0800

    profile page

--- site/next-amplify-auth/pages/_app.js
+++ site/next-amplify-auth/pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
+import { css } from "emotion";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Amplify from "aws-amplify";
+import config from "../src/aws-exports";
+Amplify.configure({
+  ...config,
+  ssr: true,
+});
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className={navStyle}>
+        <Link href="/">
+          <span className={linkStyle}>Home</span>
+        </Link>
+        <Link href="/profile">
+          <span className={linkStyle}>Profile</span>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+const linkStyle = css`
+  margin-right: 20px;
+  cursor: pointer;
+`;
+
+const navStyle = css`
+  display: flex;
+`;
