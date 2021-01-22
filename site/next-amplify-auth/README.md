66ce083d0f693196a81baa87c586fd333ca14e9d
talves <tony@3alves.com>
2021-01-22 10:43:37 -0800

    amplify bootstrap 🔥

---
+++ site/next-amplify-auth/README.md
@@ -0,0 +1,16 @@
+## Created using
+
+`npx create-next-app next-amplify-auth`
+`yarn add @aws-amplify/cli -D` could create global install, but I choose to include it here
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
--- site/next-firebase-auth/pages/_app.js
+++ site/next-firebase-auth/pages/_app.js
@@ -3,11 +3,13 @@ import { FirebaseProvider } from "firebase-react-provider";
 
 /* Replace the config values with your firebase config */
 const config = {
-  apiKey: "AIza...................................",
-  authDomain: "your-app-name.firebaseapp.com",
-  databaseURL: "https://your-app-name.firebaseio.com",
-  projectId: "your-app-name",
-  // storageBucket: "your-app-name.appspot.com",
+  apiKey: "AIzaSyBT3uEGmRADTBwE0lY7WKv-fk6l33fHILc",
+  authDomain: "garden-posts-demo.firebaseapp.com",
+  databaseURL: "https://garden-posts-demo.firebaseio.com",
+  projectId: "garden-posts-demo",
+  // storageBucket: "garden-posts-demo.appspot.com",
+  appId: "1:316947097235:web:7bca27def6b7275d658f66",
+  measurementId: "G-PX67HVDJLX",
 };
 
 function Application({ Component, pageProps }) {
